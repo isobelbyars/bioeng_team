fea7ce4cbc1cbb544d18cc876cd1c593232d2956
Oliver G <oliver.gallo@student.unimelb.edu.au>
2022-05-23 13:47:09 +1000

    Q2C
    Issues with ODE Sys for Vdot2, Xdot2 -> VdotN, XdotN

---
+++ ODESysHivCR.m
@@ -0,0 +1,33 @@
+function dydt=ODESysHivCR(t,y,Rc)
+% Name: Oliver Gallo
+% Date: 20220523
+% Description: Custom Encoding of HIV Cross-Reactivity ODE System
+%
+% Inputs:
+%   t: Current Time
+%   y: Current Viral Concentration vector [Vn;Xn;...;Z]Length is 2*Strain
+%   count + 1 (Z)
+%   Rc: Rate Constant vector [r;p;q;c;k;b]
+% Output:
+%   dydt: ODE Column Vector
+%
+
+    % Rate Constants
+    r = Rc(1);p = Rc(2);q = Rc(3);c = Rc(4);k = Rc(5);b = Rc(6);
+
+    dydt = zeros(length(y),1);
+
+    % Generate all Vi, Xi required
+    for i=1:2:length(y)-1
+        dydt(i) = r*y(i) - p*y(i)*y(i+1) - q*y(i)*y(end);
+        dydt(i+1) = c*y(i) - b*y(i+1);
+    end
+    
+    % Define V as sum of all Vi
+    idxVi = (1:2:length(y)-2); % Indexes of Vi in dydt
+    V = sum(dydt(idxVi));
+
+    % Define Z equation
+    dydt(end) = k*V - b*y(end);
+
+end
