d8ea4e024f538982e1d83ece43baa091edac0d21
Oliver G <oliver.gallo@student.unimelb.edu.au>
2022-05-24 12:37:59 +1000

    Helper Data Writing Function

---
+++ dataWrite.m
@@ -0,0 +1,29 @@
+function dataWrite(OutData,fullName)
+% Name: Oliver Gallo
+% Date: 20220524
+% Description: Helper Function to write data to file and avoid overwriting
+%   existing data files. Writes to current directory.
+%
+% Inputs:
+%   OutData: Cell array of content to write to file
+%   fullName: File name (including extension)
+%   (t0) and end (tf) of interval
+% Output:
+%   None
+%
+
+% Base case of file not existing
+    if ~isfile(fullName)
+        writecell(OutData,fullName)
+    else
+        n = 2; % Digit to append to file name
+        spltName = strsplit(fullName,'.');
+        while true
+            newName = sprintf('%s%i.%s',spltName{1},n,spltName{2});
+            if ~isfile(newName)
+                writecell(OutData,newName)
+                break
+            end
+        end
+    end
+end
